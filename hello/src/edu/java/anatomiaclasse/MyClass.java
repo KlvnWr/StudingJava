52d247e1cb564b8686c38346818bf12b00702a49
KlvnWr <kelvin.wartanabe@gmail.com>
2024-08-18 23:07:18 -0300

    Till the end

---
+++ hello/src/edu/java/anatomiaclasse/MyClass.java
@@ -0,0 +1,16 @@
+package edu.java.anatomiaclasse;
+
+public class MyClass {
+    public static void main(String[] args) {
+        String primeiroNome = "Kelvin";
+        String segundoNome = "Watanabe";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do metodo: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
